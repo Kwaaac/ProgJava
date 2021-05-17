9f2177a03611c032ee715018e7a61e61f63af942
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2021-05-17 14:54:52 +0200

    TP8 - constructor update

--- TP8/src/fr/umlv/cooperative/Apple.java
+++ TP8/src/fr/umlv/cooperative/Apple.java
@@ -1,6 +1,16 @@
 package fr.umlv.cooperative;
 
+import java.util.Objects;
+
 public record Apple(int weight, AppleKind variety) implements Fruit {
+
+    public Apple {
+        Objects.requireNonNull(variety);
+        if (weight < 0) {
+            throw new IllegalArgumentException("The weight must be positive");
+        }
+    }
+
     @Override
     public String toString() {
         return variety + " " + weight + " g ";
--- TP8/src/fr/umlv/cooperative/Pear.java
+++ TP8/src/fr/umlv/cooperative/Pear.java
@@ -1,6 +1,13 @@
 package fr.umlv.cooperative;
 
 public record Pear(int juiceFactor) implements Fruit {
+
+    public Pear {
+        if(juiceFactor < 0 || juiceFactor > 9){
+            throw new IllegalArgumentException("The juice factor must be between 0 and 9");
+        }
+    }
+
     @Override
     public int price() {
         return juiceFactor * 3;
